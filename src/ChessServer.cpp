bd983ea754dd3c9820e374a7306425d128d189f1
Julian Sobczuk <julek@sobczuk.pl>
2025-03-31 15:42:22 +0200

    Network basics

---
+++ src/ChessServer.cpp
@@ -0,0 +1,13 @@
+#include "Player/ChessServer.h"
+#include "httplib.h"
+
+void ChessServer::make_move() {
+  std::string move_to_make;
+
+  httplib::Server svr;
+  svr.Get("/move/:move", [](const httplib::Request& req, httplib::Response& res) {
+    auto made_move = req.path_params.at("move");
+    res.set_content(move_to_make, "text/plain");
+  });
+  svr.listen("localhost", port);
+}
---
+++ src/PlayerOnline.cpp
@@ -0,0 +1,17 @@
+#include "Player/PlayerOnline.h"
+#include "httplib.h"
+#include <string>
+
+void PlayerOnline::make_move() {
+  std::string move; // TODO move to func 
+  httplib::Client client(ip, port);
+
+  int timeout = 60 * 5; // max 5 min na ruch
+  client.set_max_timeout(timeout);
+
+  auto res = client.Get("/move/" + move);
+
+  if (res) {
+      
+  }
+}
