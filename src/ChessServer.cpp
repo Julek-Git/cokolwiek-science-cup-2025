b686c2516d00e5d9daf11f905ab08acf1db9a549
Julian Sobczuk <julek@sobczuk.pl>
2025-04-01 20:54:41 +0200

    net isnt working

--- src/ChessServer.cpp
+++ src/ChessServer.cpp
@@ -2,12 +2,12 @@
 #include "httplib.h"
 
 void ChessServer::make_move() {
-  std::string move_to_make;
+  // std::string move_to_make;
 
-  httplib::Server svr;
-  svr.Get("/move/:move", [](const httplib::Request& req, httplib::Response& res) {
-    auto made_move = req.path_params.at("move");
-    res.set_content(move_to_make, "text/plain");
-  });
-  svr.listen("localhost", port);
+  // httplib::Server svr;
+  // svr.Get("/move/:move", [](const httplib::Request& req, httplib::Response& res) {
+  //   auto made_move = req.path_params.at("move");
+  //   res.set_content(move_to_make, "text/plain");
+  // });
+  // svr.listen("localhost", port);
 }
--- src/PlayerOnline.cpp
+++ src/PlayerOnline.cpp
@@ -3,15 +3,15 @@
 #include <string>
 
 void PlayerOnline::make_move() {
-  std::string move; // TODO move to func 
-  httplib::Client client(ip, port);
+  // std::string move; // TODO move to func 
+  // httplib::Client client(ip, port);
 
-  int timeout = 60 * 5; // max 5 min na ruch
-  client.set_max_timeout(timeout);
+  // int timeout = 60 * 5; // max 5 min na ruch
+  // client.set_max_timeout(timeout);
 
-  auto res = client.Get("/move/" + move);
+  // auto res = client.Get("/move/" + move);
 
-  if (res) {
+  // if (res) {
       
-  }
+  // }
 }
